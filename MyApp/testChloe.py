c70c1fea174697ba178f5ac41d921941b2290a62
chloemorin12 <chloe.morin.6@ulaval.ca>
2025-04-06 14:59:33 -0400

    Algo_puissance_update

--- MyApp/testChloe.py
+++ MyApp/testChloe.py
@@ -25,33 +25,104 @@ def gaussian_2d(coords, A, x0, y0, sigma_x, sigma_y, offset):
     return A * np.exp(-((x - x0)**2 / (2 * sigma_x**2) + (y - y0)**2 / (2 * sigma_y**2))) + offset
 
 
+# Fonction de transfert invers 2e ordre 
 
-file_path = r"C:\Users\chloe\echellon.xlsx"
+def inverse_transfer_function( Gain, w_n, z):
+    T1 = (2*z)/w_n
+    T2 = 1/w_n**2
+    G = Gain
+
+    Coef1 = (1 + T1/0.15 + T2/(0.15**2))/ G
+    Coef2 = (T1/0.15 - 2*T2/(0.15**2))/G
+    Coef3 = (T2/(0.15**2 * G))
+
+    return [Coef1, Coef2, Coef3]
+    
+
+
+
+file_path = r"C:\Users\chloe\5mm_bas_validation.xlsx"
 df = pd.read_excel(file_path)
 
-thermistance = df.iloc[:, :9]
-ref = df.iloc[:, 9]
-#t = df.iloc[:, 10]
-t = pd.to_numeric(df.iloc[:, 10], errors='coerce')
-thermistance = thermistance.apply(lambda col: col -ref, axis=0)
+
+
+thermistance1 = df.iloc[:, :1]
+ref = df.iloc[:, 2]
+t = pd.to_numeric(df.iloc[:, 3], errors='coerce')
+thermistance1 = thermistance1.apply(lambda col: col -ref, axis=0)  #subtract(ref, axis=0) #
+
+
+
+
 
 
 # paramètre de la fonction de transfert (2e ordre)
-a_0 = 3286
-b_0 = -6450
-c_0 = 3245
-
-k = 1.965
-tau = 15.35
 
 def puissance_calcul(temps, te):
 
 
     puissance = []
 
+    #a_0, a_1, a_2 = inverse_transfer_function(0.85, 0.13, 1)
+    #b_0, b_1, b_2 = inverse_transfer_function(0.86, 0.12, 1)
+    c_0, c_1, c_2 = inverse_transfer_function(19.26677379649072,  0.05307996738598514, 0.8342156871242603)
+    #d_0, d_1, d_2 = inverse_transfer_function(0.73, -0.08, -0.52)
+
     # Modifier selon le profil de température
     #moyenne différence temp pour avoir énergie
     for i in range(2, len(temps)):
+        
+
+        #E = temps.iloc[i, :9].mean()
+        #dt = te.iloc[i] - te.iloc[i-1]
+        #dE = (temps.iloc[i, :9].mean() - temps.iloc[i-1, :9].mean())/dt
+        #P_t = E/k + (tau/k)*dE
+
+            # 2e ordre
+        T_k = temps.iloc[i].mean()
+        T_k_1 = temps.iloc[i-1].mean()
+        T_k_2 = temps.iloc[i-2].mean()
+        # Fctn_de_transfert 2e ordre
+        P_t = c_0*T_k + c_1*T_k_1 + c_2*T_k_2
+
+            
+        puissance.append(P_t)
+
+    puissance = [np.nan, np.nan] + puissance
+    return puissance
+    
+df['Puissance'] = puissance_calcul(thermistance1, t)
+
+output_file = 'thermistance_echellon_with_puissance_dimanche_2.xlsx'
+df.to_excel(output_file, index=False)
+print(f"Data with power column saved to {output_file}")
+
+
+
+
+'''# Version / thermistance
+thermistance = df.iloc[:, :1]
+ref = df.iloc[:, 2]
+t = pd.to_numeric(df.iloc[:, 3], errors='coerce')
+thermistance = thermistance.apply(lambda col: col -ref, axis=0)
+
+
+# paramètre de la fonction de transfert (2e ordre)
+
+def puissance_calcul(temps, te):
+
+
+    puissance = []
+
+    a_0, a_1, a_2 = inverse_transfer_function(0.85, 0.13, 1)
+    b_0, b_1, b_2 = inverse_transfer_function(0.86, 0.12, 1)
+    c_0, c_1, c_2 = inverse_transfer_function(2.14, -0.08, -0.52)
+    d_0, d_1, d_2 = inverse_transfer_function(0.73, -0.08, -0.52)
+    
+    # Modifier selon le profil de température
+    #moyenne différence temp pour avoir énergie
+    for i in range(2, len(temps)):
+
         #E = temps.iloc[i, :9].mean()
         #dt = te.iloc[i] - te.iloc[i-1]
         #dE = (temps.iloc[i, :9].mean() - temps.iloc[i-1, :9].mean())/dt
@@ -72,8 +143,11 @@ def puissance_calcul(temps, te):
     
 df['Puissance'] = puissance_calcul(thermistance, t)
 
-output_file = 'thermistance_echellon_with_puissance.xlsx'
+output_file = 'thermistance_echellon_with_puissance_dim.xlsx'
 df.to_excel(output_file, index=False)
+print(f"Data with power column saved to {output_file}")
+'''
+
 
 
 
