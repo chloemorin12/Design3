c38524ecda08809c166db0611bc41457f429f1a9
Alexis Giroux <117380232+algir123@users.noreply.github.com>
2025-04-08 21:04:39 -0400

    AcquisitionClass_Wavelenght_V2

--- MyApp/AcquisitionClass.py
+++ MyApp/AcquisitionClass.py
@@ -8,13 +8,12 @@ class Acquisition:
     def __init__(self):
         self.voltage_data = np.full((256, 1), np.nan)
         self.thermistor = np.full((256, 2), np.nan)
-        self.max_value = 256 
-        #self.delay = 0.001
-        self.value = 0
         duration = 0.01
         self.sample_rate = 1000
         self.samples_to_read = int(duration * self.sample_rate)
-        self.real_thermistor_positions = [
+        
+    def assign_thermistor_positions(self):
+        real_thermistor_positions = [
             [-6.25, -10.825317547305483], [-3.125, -10.825317547305483], [0.0, -10.825317547305483],
             [3.125, -10.825317547305483], [6.25, -10.825317547305483], [-7.8125, -8.118988160479113],
             [-4.6875, -8.118988160479113], [-1.5625, -8.118988160479113], [1.5625, -8.118988160479113],
@@ -34,34 +33,19 @@ class Acquisition:
             [1.5625, 8.118988160479113], [4.6875, 8.118988160479113], [7.8125, 8.118988160479113],
             [-6.25, 10.825317547305483], [-3.125, 10.825317547305483], [0.0, 10.825317547305483],
             [3.125, 10.825317547305483], [6.25, 10.825317547305483]]
-        self.liste_thermistor_values = [
+        liste_thermistor_values = [
                            66,82,113,48,32,97,67,98,49,16,0,81,68,83,114,33,17,
                            1,65,69,84,99,50,34,18,2,112,70,85,100,115,51,35,19,
                            3,71,86,101,116,52,36,20,4,87,102,117,53,37,21,5,103,
                            118,54,38,22,6,119,55,39,23,7]
-        
-    def assign_thermistor_positions(self):
-        for i in range(len(self.real_thermistor_positions)):
-            position = self.liste_thermistor_values[i]
-            self.thermistor[position] = self.real_thermistor_positions[i]
+        for i in range(len(real_thermistor_positions)):
+            position = liste_thermistor_values[i]
+            self.thermistor[position] = real_thermistor_positions[i]
         return self.thermistor
-
-    def set_daq_output(self):
-        with nidaqmx.Task() as do_task:
-            do_task.do_channels.add_do_chan("Dev1/port0/line0:7")
-            do_task.write(self.value, auto_start=True)
-
-    def read_voltage(self):
-        with nidaqmx.Task() as task:
-            task.ai_channels.add_ai_voltage_chan("Dev1/ai7")
-            task.timing.cfg_samp_clk_timing(
-                rate=self.sample_rate,
-                sample_mode=AcquisitionType.FINITE,
-                samps_per_chan=self.samples_to_read)
-            data = task.read(number_of_samples_per_channel=self.samples_to_read)
-            return(np.mean(data))
         
     def Power_thermistor(self):
+        max_value = 256
+        value = 0
         start_time = time.perf_counter()
         with nidaqmx.Task() as do_task, nidaqmx.Task() as ai_task:
             do_task.do_channels.add_do_chan("Dev1/port0/line0:7")
@@ -71,13 +55,13 @@ class Acquisition:
                 sample_mode=AcquisitionType.FINITE,
                 samps_per_chan=self.samples_to_read)
 
-            for i in range(self.max_value):
-                self.value = i
-                binary_str = format(self.value, '08b')
+            for i in range(max_value):
+                value = i
+                binary_str = format(value, '08b')
 
                 if binary_str[-4] == '1':
-                    self.value += 8
-                    binary_str = format(self.value, '08b')
+                    value += 8
+                    binary_str = format(value, '08b')
                 if binary_str[-8] == '1':
                     data = np.hstack((self.thermistor, self.voltage_data))
                     data = data[~np.isnan(data).any(axis=1)]
@@ -95,7 +79,7 @@ class Acquisition:
                     print(f"Temps total: {stop_time - start_time:.2f} s")
                     return self.voltage_data
                 
-                do_task.write(self.value, auto_start=True)
+                do_task.write(value, auto_start=True)
                 data = ai_task.read(number_of_samples_per_channel=self.samples_to_read)
                 self.voltage_data[i] = np.mean(data)
                 
